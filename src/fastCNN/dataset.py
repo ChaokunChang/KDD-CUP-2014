b68b766ef1b1faf40f9651f7f42378f666d91c56
fdcck <16307130138@fudan.edu.cn>
2019-06-14 13:37:10 +0800

    pylint

--- src/fastCNN/dataset.py
+++ src/fastCNN/dataset.py
@@ -20,54 +20,20 @@ from fastNLP import Instance
 from fastNLP import Vocabulary
 from fastNLP import Const
 
+
 def combine_whitespace(s):
     return s.split()
 
-def tokenize2(data_train,data_test,max_num=100000):
-    train = []
-    test = []
-    for i in range(len(data_train)):
-        text = data_train[i]
-        if text=="":
-            print("Empty text.{0}".format(i))
-        elif text is None:
-            print("None type text.")
-        newtext=""
-        for c in text:
-            if c not in string.punctuation:
-                newtext += c
-            else:
-                newtext += ' '
-        newtext = combine_whitespace(newtext.lower())
-        if newtext is None:
-            train.append(newtext)
-    
-    for i in range(len(data_test)):
-        text = data_test[i]
-        if text=="":
-            print("Empty text.{0}".format(i))
-        elif text is None:
-            print("None type text.")
-        newtext=""
-        for c in text:
-            if c not in string.punctuation:
-                newtext += c
-            else:
-                newtext += ' '
-        newtext = combine_whitespace(newtext.lower())
-        if newtext is not None:
-            test.append(newtext)
-    return train,test
 
 def tokenize(data):
     new_data = []
     for i in range(len(data)):
         text = data[i]
-        if text=="":
+        if text == "":
             print("Empty text.{0}".format(i))
         elif text is None:
             print("None type text.")
-        newtext=""
+        newtext = ""
         for c in text:
             if c not in string.punctuation:
                 newtext += c
@@ -79,16 +45,18 @@ def tokenize(data):
     return new_data
 
 
-def text2multi_hot(words,vocab_size,word2index=None):
+def text2multi_hot(words, vocab_size, word2index=None):
     multi_hot_vector = [0]*(vocab_size)
     for word in words:
         multi_hot_vector[word] = 1
     return multi_hot_vector
 
-def class2target(class_type,class_num):
+
+def class2target(class_type, class_num):
     target = [0]*class_num
     target[class_type] = 1
     return target
+
 
 class TextData():
     data_src = "all_data"
@@ -107,121 +75,123 @@ class TextData():
     vocab = None
     vocab_size = 0
 
-    def __init__(self,data_src="all_data",min_count=10,seq_limit=None):
+    def __init__(self, data_src="all_data", min_count=10, seq_limit=None):
         self.data_src = data_src
         self.min_count = min_count
         if seq_limit is not None:
             self.seq_limit = seq_limit
 
-    def find_max_len(self,words):
-        self.max_seq_len = max( len(words),self.max_seq_len )
+    def find_max_len(self, words):
+        self.max_seq_len = max(len(words), self.max_seq_len)
 
-    def seq_regularize(self,words):
+    def seq_regularize(self, words):
         wlen = len(words)
-        if wlen<self.max_seq_len:
+        if wlen < self.max_seq_len:
             return [0]*(self.max_seq_len-wlen) + words
         else:
             return words[:self.max_seq_len]
 
-    def fetch_csv(self,path=None):
+    def fetch_csv(self, path=None):
         print("Loading data from {} .......".format(path))
-        df = pd.read_csv(path,header=0)
+        df = pd.read_csv(path, header=0)
 
         train_df = df[df['split'] == 'train']
         val_df = df[df['split'] == 'val']
         test_df = df[df['split'] == 'test']
 
         # text_vars=["title", "short_description", "need_statement", "essay"]
-        text_vars = "essay" # only select the essay column
+        text_vars = "essay"  # only select the essay column
         print("tokenize train set")
-        train_input = tokenize( train_df[ text_vars ].values )
+        train_input = tokenize(train_df[text_vars].values)
         print("tokenize val set")
-        val_input = tokenize( val_df[ text_vars ].values )
+        val_input = tokenize(val_df[text_vars].values)
         print("tokenize test set")
-        test_input = tokenize( test_df[ text_vars ].values )
+        test_input = tokenize(test_df[text_vars].values)
 
         target_var = "y"
-        train_target = train_df[ target_var ].values
-        val_target = val_df[ target_var ].values
-        test_target = test_df[ target_var ].values
+        train_target = train_df[target_var].values
+        val_target = val_df[target_var].values
+        test_target = test_df[target_var].values
 
         assert (self.class_num == 2)
 
         # Building Fastnlp dataset.
         print("Building Fastnlp dataset.")
-        self.train_set = DataSet({"text":train_input,"class":train_target})
-        self.val_set = DataSet({"text":val_input,"class":val_target})
-        self.test_set = DataSet({"text":test_input,"class":test_target})
-        
+        self.train_set = DataSet({"text": train_input, "class": train_target})
+        self.val_set = DataSet({"text": val_input, "class": val_target})
+        self.test_set = DataSet({"text": test_input, "class": test_target})
+
         # Building Fastnlp vocabulary...
         print("Building Fastnlp vocabulary.")
         self.vocab = Vocabulary(min_freq=self.min_count)
-        self.train_set.apply(lambda x : [self.vocab.add_word(word) for word in x['text']])
+        self.train_set.apply(
+            lambda x: [self.vocab.add_word(word) for word in x['text']])
         self.vocab.build_vocab()
         self.vocab.build_reverse_vocab()
         self.vocab_size = len(self.vocab)
         # Building multi-hot-vector for train_set and test_set.
         print("Building id-presentation for train_set and test_set.")
-        self.vocab.index_dataset(self.train_set,self.val_set,self.test_set,field_name='text',new_field_name='words')
-        
-        self.train_set.apply_field(lambda x : len(x),field_name='words',new_field_name='seq_len')
-        self.val_set.apply_field(lambda x : len(x),field_name='words',new_field_name='seq_len')
-        self.test_set.apply_field(lambda x : len(x),field_name='words',new_field_name='seq_len')
-        self.train_set.apply_field(self.find_max_len,field_name='words')
+        self.vocab.index_dataset(self.train_set, self.val_set,
+                                 self.test_set, field_name='text', new_field_name='words')
+
+        self.train_set.apply_field(lambda x: len(
+            x), field_name='words', new_field_name='seq_len')
+        self.val_set.apply_field(lambda x: len(
+            x), field_name='words', new_field_name='seq_len')
+        self.test_set.apply_field(lambda x: len(
+            x), field_name='words', new_field_name='seq_len')
+        self.train_set.apply_field(self.find_max_len, field_name='words')
 
         print(self.max_seq_len)
-        self.max_seq_len = min(self.max_seq_len,self.seq_limit)
+        self.max_seq_len = min(self.max_seq_len, self.seq_limit)
 
-        self.train_set.apply_field(self.seq_regularize,field_name='words',new_field_name='words')
-        self.val_set.apply_field(self.seq_regularize,field_name='words',new_field_name='words')
-        self.test_set.apply_field(self.seq_regularize,field_name='words',new_field_name='words')
+        self.train_set.apply_field(
+            self.seq_regularize, field_name='words', new_field_name='words')
+        self.val_set.apply_field(
+            self.seq_regularize, field_name='words', new_field_name='words')
+        self.test_set.apply_field(
+            self.seq_regularize, field_name='words', new_field_name='words')
         # self.train_set.apply(lambda x : text2multi_hot(x['words'],self.vocab_size),new_field_name="input")
         # self.val_set.apply(lambda x : text2multi_hot(x['words'],self.vocab_size),new_field_name='input')
         # self.test_set.apply(lambda x : text2multi_hot(x['words'],self.vocab_size),new_field_name='input')
-        
+
         # Building target-vector for train_set and test_set.
         print("Building target-vector for train_set and test_set.")
-        self.train_set.apply(lambda x : int(x['class']),new_field_name="target",is_target=True)
-        self.val_set.apply(lambda x : int(x['class']),new_field_name="target",is_target=True)
-        self.test_set.apply(lambda x : int(x['class']),new_field_name="target",is_target=True)
+        self.train_set.apply(lambda x: int(
+            x['class']), new_field_name="target", is_target=True)
+        self.val_set.apply(lambda x: int(
+            x['class']), new_field_name="target", is_target=True)
+        self.test_set.apply(lambda x: int(
+            x['class']), new_field_name="target", is_target=True)
         # self.train_set.apply(lambda x : class2target(x['class'],self.calss_num),new_field_name="target")
         # self.test_set.apply(lambda x : class2target(x['class'],self.calss_num),new_field_name="target")
-        
-    def fetch_data(self,path=None):
+
+    def fetch_data(self, path=None):
         if self.data_src == "all_data":
             # Loading 20newsgroups data and tokenize.
             self.fetch_csv(path)
         else:
             print("No legal data src type:{} ...".format(self.data_src))
-            assert(0==1)
-        
+            assert(0 == 1)
+
         self.train_size = self.train_set.get_length()
         self.val_size = self.val_set.get_length()
         self.test_size = self.test_set.get_length()
-        return self.train_size, self.val_size ,self.test_size
+        return self.train_size, self.val_size, self.test_size
+
 
 if __name__ == "__main__":
     data_dir = 'KDD2014\data'
     data = TextData(data_src='all_data')
-    print(data.fetch_data(path=opj(data_dir,'suboutcome.csv')) )
+    print(data.fetch_data(path=opj(data_dir, 'suboutcome.csv')))
     len_lst = data.train_set.get_field('seq_len')
-    plt.hist(len_lst,bins=500)
+    plt.hist(len_lst, bins=500)
     plt.title("Essay_Length_distribution")
     # plt.show()
     plt.savefig("Essay_Length_distribution.png")
     print("Test done.")
-    
+
     print('Saving vocab(TextData)...')
     with open(os.path.join(data_dir, 'vocab.data'), 'wb') as fout:
         pkl.dump(data, fout)
     print('Done with preparing!')
-    
-
-
-
-
-
-
-
-
-
